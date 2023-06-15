b8cd98bef92cf483d3fc33186a64d336eadc3991
DuBoris Smith <duboris.smith@hotmail.com>
2023-06-15 14:04:20 -0500

    Min add to function

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
+import { useState } from 'react'
+import './index.css'
+
 // Write your Color component here
-const Color = (props) => {
-  return <div className={props.color}></div>
+const Color = (props, setSelectedColor) => {
+
+  return (
+    <div 
+      className={props.color}
+      onClick={() => setSelectedColor(props)}
+    ></div>
+  )
 }
+
+
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
+  //if (selectedColor === setSelectedColor) 
+  //setSelectedColor(selectedColor);
+  //const navColor = (appColor) => {setSelectedColor(appColor)};
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
       <div id="colors-list"> 
-          <Color color="red"/>
-          <Color color="black"/>
-          <Color color="blue"/>
+          <Color color="red" setSelectedColor={setSelectedColor}/>
+          <Color color="black" setSelectedColor={setSelectedColor}/>
+          <Color color="blue" setSelectedColor={setSelectedColor}/>
       </div>
     </div>
   );
